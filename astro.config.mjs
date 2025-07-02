bf3254b33e2a54c2d4c7b1574d747fcc2e215ab5
alberba <83814585+alberba@users.noreply.github.com>
2025-07-02 13:44:44 +0200

    Actualiza dependencias de Astro y Tailwind CSS, y ajusta estilos en DropZone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   base: "/lorasuib/",
   outDir: "./build/lorasuib",
+
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  },
 });
